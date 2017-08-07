b9a05815d2d773d7bd872615b8cd071638339840
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-08-07 15:02:46 -0700

    feat(Nav): Add initial top nav

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,38 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 
 import './index.styl';
-
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem'
-    }}
-    >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem'
-      }}
-      >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          style={{
-            color: 'white',
-            textDecoration: 'none'
-          }}
-          to='/'
-          >
-          JamCommerce
-        </Link>
-      </h1>
-    </div>
-  </div>
-);
+import Nav from './Nav';
 
 export default function TemplateWrapper({ children }) {
   return (
@@ -42,18 +13,11 @@ export default function TemplateWrapper({ children }) {
           { name: 'description', content: 'Sample' },
           { name: 'keywords', content: 'sample, something' }
         ]}
-        title='Gatsby Default Starter'
+        title='JamCommerce'
       />
-      <Header />
-      <div
-        style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '0px 1.0875rem 1.45rem',
-          paddingTop: 0
-        }}
-        >
-        {children()}
+      <Nav />
+      <div>
+        { children() }
       </div>
     </div>
   );
