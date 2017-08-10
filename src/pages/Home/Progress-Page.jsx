60009d3c002de2eb50b46b7b6c33ee40dbd1f251
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-08-10 00:48:58 -0700

    feat(Home): Add home page

---
+++ src/pages/Home/Progress-Page.jsx
@@ -0,0 +1,55 @@
+import React, { PropTypes } from 'react';
+import { AnimatedView } from 'react-view-pager';
+
+const propTypes = {
+  className: PropTypes.string,
+  index: PropTypes.number,
+  onClick: PropTypes.func
+};
+
+const colors = [
+  '#209D22',
+  '#106CCC',
+  '#C1146B',
+  '#11BDBF',
+  '#8A19EA'
+];
+
+export default function ProgressPage({
+  className,
+  index,
+  onClick
+}) {
+  return (
+    <AnimatedView
+      animations={[{
+        prop: 'scale',
+        stops: [
+          [ -300, 0.75 ],
+          [ 0, 1 ],
+          [ 300, 0.75 ]
+        ]
+      }, {
+        prop: 'opacity',
+        stops: [
+          [ -300, 0.5 ],
+          [ 0, 1 ],
+          [ 300, 0.5 ]
+        ]
+      }, {
+        prop: 'backgroundColor',
+        stops: [
+          [ -300, '#cccccc' ],
+          [ 0, colors[ index ] ],
+          [ 300, '#cccccc' ]
+        ]
+      }]}
+      className={ className }
+      index={ index }
+      key={ index }
+      onClick={ onClick }
+    />
+  );
+}
+ProgressPage.displayName = 'ProgressPage';
+ProgressPage.propTypes = propTypes;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,1 @@
-import React from 'react';
-
-export default function IndexPage() {
-  return (
-    <div>
-      <h1>Foo</h1>
-    </div>
-  );
-}
-
+export { default } from './Home';
