59be159d6f58d24b7ec254f2b64d378952695eb6
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-08-14 13:50:17 -0700

    fix(Callout): Style

--- src/pages/Home/Callout.jsx
+++ src/pages/Home/Callout.jsx
@@ -11,18 +11,18 @@ const propTypes = {};
 export default function CallOut() {
   return (
     <div className={ cx('callout') }>
-      <div><h1>Nothing to Hack</h1></div>
-      <div>
-Because your app is rendered in pure static HTML, there is virtually no risk of
-SQL injection, malware attacks, or other nefarious hackery.
-      </div>
-      <LearnMore />
-      <div>
-        <img
-          className={ cx('img') }
-          src={ callout }
-        />
-      </div>
+      <header className={ cx('copy') }>
+        <h1 className={ cx('title') }>Nothing to Hack</h1>
+        <p>
+          Because your app is rendered in pure static HTML, there is virtually
+          no risk of SQL injection, malware attacks, or other nefarious hackery.
+        </p>
+        <LearnMore />
+      </header>
+      <img
+        className={ cx('img') }
+        src={ callout }
+      />
     </div>
   );
 }
