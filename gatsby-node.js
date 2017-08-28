167f58bcead159ed902fd764c0f5c1c2a50973b7
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-08-27 19:25:47 -0700

    refactor(gatsby): Remove comment

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,28 +1,6 @@
 const path = require('path');
 const dedent = require('dedent');
 
-// exports.sourceNodes = (
-//   { boundActionCreators: { createNode } },
-//   options,
-//   done,
-// ) => {
-//   createNode({
-//     id: 'Product - Callout - Shoes',
-//     internal: {
-//       mediaType: 'application/json',
-//       content: JSON.stringify({
-//         calloutCopy: dedent`
-//         With the git-centric static workflow of Go Commerce, you can securely
-//           track your inventory, purchases, user history, and admin
-//           authentication — all from a lean, robust, and blazing fast
-//           architecture.
-//         `,
-//       }),
-//     },
-//   });
-//   done();
-// };
-
 exports.createPages = ({ boundActionCreators: { createPage } }) => {
   const productTemplate = path.resolve('src/features/Product/index.js');
   createPage({
