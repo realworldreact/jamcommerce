9a2cb48b66f4d30f9a10588ca002c57955ccde83
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-09-21 23:30:48 -0700

    feat: Initial checkout page

---
+++ src/features/Checkout/Checkout.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import classnames from 'classnames/bind';
+
+import styles from './checkout.module.styl';
+
+const cx = classnames.bind(styles);
+const propTypes = {};
+
+export default function Checkout() {
+  return (
+    <div className={ cx('checkout') }>
+      <header className={ cx('header') }>
+        <h4>
+           Check Out
+        </h4>
+      </header>
+      <div className={ cx('container') }>
+        <nav className={ cx('nav') }>
+          <ul className={ cx('nav-list') }>
+            <li className={ cx('underline') }>Address</li>
+            <li>Billing</li>
+            <li>Confirm</li>
+          </ul>
+        </nav>
+        <div className={ cx('content') }>
+          Hello
+        </div>
+      </div>
+    </div>
+  );
+}
+Checkout.displayName = 'Checkout';
+Checkout.propTypes = propTypes;
---
+++ src/features/Checkout/index.js
@@ -0,0 +1,1 @@
+export { default } from './Checkout.jsx';
