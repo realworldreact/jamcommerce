b6d1ae2464889306087e8afe341d1a711ac6dc58
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-10-28 22:10:05 -0700

    feat(Checkout): Add checkout table to confirm

--- src/features/Checkout/Confirm.jsx
+++ src/features/Checkout/Confirm.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 import classnames from 'classnames/bind';
 
 import styles from './checkout.module.styl';
+import { Table } from '../Cart';
 
 const cx = classnames.bind(styles);
 const propTypes = {};
 
 export default function Confirm() {
-  return <div className={ cx('confirm') }>confirm</div>;
+  return (
+    <div className={ cx('confirm') }>
+      <header>
+        <h5>Please Review your cart order</h5>
+      </header>
+      <Table />
+      <footer className={ cx('footer') }>
+        <button className={ cx('next-button') }>
+          Confirm Purchase
+        </button>
+      </footer>
+    </div>
+  );
 }
 Confirm.displayName = 'Confirm';
 Confirm.propTypes = propTypes;
